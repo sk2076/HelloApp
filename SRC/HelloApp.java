d246ec6ceffb1dc9b2c31369e23962ec513ca7c2
Siva Sagar <sk2076@srmist.edu.in>
2026-03-17 21:25:20 +0530

    UC4 done

--- SRC/HelloApp.java
+++ SRC/HelloApp.java
@@ -1,5 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String defname = "World";
+
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+
+                if (i < args.length - 1) {   
+                    sb.append(", ");
+                }
+            }
+
+            System.out.println("Hello " + sb + "!");
+        } else {
+            System.out.println("Hello " + defname + "!");
+        }
     }
 }
